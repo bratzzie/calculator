700fa6d854f15775b55bd9f52cfbe12f15020381
Anastasiia Bichurina <bichurina.anastasiia@gmail.com>
2022-02-16 09:32:29 +0200

    main arithmetic operations

--- src/com/stud/calc/MainWindow.java
+++ src/com/stud/calc/MainWindow.java
@@ -3,22 +3,28 @@ package com.stud.calc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 
 public class MainWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Image image = new Image("/images/icons8-calculator-64.png");
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainWindowInterface.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        scene.setFill(Color.TRANSPARENT);
 
+        scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setResizable(false);
+        primaryStage.getIcons().add(image);
         primaryStage.setTitle("Calculator");
+
+       ((MainWindowController)fxmlLoader.getController()).initialize(primaryStage);
         primaryStage.show();
     }
 
@@ -26,3 +32,5 @@ public class MainWindow extends Application {
         launch();
     }
 }
+
+//Calculator icon by https://icons8.com Icons8>
--- src/com/stud/calc/MainWindowController.java
+++ src/com/stud/calc/MainWindowController.java
@@ -1,13 +1,70 @@
 package com.stud.calc;
 
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+public class MainWindowController {
+    @FXML private Pane titlePane;
+    @FXML private Button closeButton, minButton;
+    @FXML private Label resultOutput;
 
-public class MainWindowController implements Initializable {
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    private double x, y;
+    private double num1 = 0;
+    private String operatorInMathView = "+";
 
+    public void initialize(Stage stage) {
+        titlePane.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        titlePane.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
+
+        closeButton.setOnMouseClicked(mouseEvent -> stage.close());
+        minButton.setOnMouseClicked(mouseEvent -> stage.setIconified(true));
+    }
+
+    @FXML
+    void onOperandClicked(MouseEvent event) {
+        int operand = Integer.parseInt(((Label)event.getSource()).getId().replace("btn", ""));
+        resultOutput.setText(Double.parseDouble(resultOutput.getText()) == 0 ? String.valueOf((double) operand)
+                : String.valueOf(Double.parseDouble(resultOutput.getText())*10+operand)); // *10 - to avoid the collapse of numbers
+    }
+
+    @FXML
+    void onOperatorClicked(MouseEvent event) {
+        String operator = ((Label)event.getSource()).getId().replace("btn","");
+        if(operator.equals("Equals")) {
+            double num2 = Double.parseDouble(resultOutput.getText());
+            switch (operatorInMathView) {
+                case "+" -> resultOutput.setText(String.valueOf(num1+num2));
+                case "-" -> resultOutput.setText(String.valueOf(num1-num2));
+                case "*" -> resultOutput.setText(String.valueOf(num1*num2));
+                case "/" -> resultOutput.setText(String.valueOf(num1/num2));
+                case "%" -> resultOutput.setText(String.valueOf(num1%num2));
+            }
+            operatorInMathView = ".";
+        }
+        else if(operator.equals("MC") || operator.equals("AC")) {
+            resultOutput.setText(String.valueOf(0));
+            operatorInMathView = ".";
+        }
+        else {
+            switch (operator) {
+                case "Plus" -> operatorInMathView = "+";
+                case "Minus" -> operatorInMathView = "-";
+                case "Multiply" -> operatorInMathView = "*";
+                case "Divide" -> operatorInMathView = "/";
+                case "Mod" -> operatorInMathView = "%";
+            }
+            num1 = Double.parseDouble(resultOutput.getText());
+            resultOutput.setText(String.valueOf(0));
+        }
     }
 }
